ed856812b3553b0ab93ef60daf7cffc5c89da655
lsm <lsm@thetelos.net>
2023-03-07 01:05:22 +0900

    animation: 메인 페이지 하단 텍스트 애니메이션 리펙

--- src/components/main/MainBottomAnimate/index.tsx
+++ src/components/main/MainBottomAnimate/index.tsx
@@ -9,29 +9,18 @@ const MainBottomAnimate = () => {
       duration: 500,
       anchorPlacement: 'center-center',
       initClassName: 'aos-init',
+      mirror: true,
+      offset: -300,
     })
-  })
+  }, [])
 
   return (
-    // <section
-    //   data-aos="animate-slogun"
-    //   className="flex items-center justify-center w-full h-[110vh] sm:h-[130vh] lg:h-[150vh] px-30 sm:px-40 lg:px-50"
-    // >
-    //   <article>
-    //     <h2 className="font-extrabold text-center font-open-sans text-40 sm:text-60 md:text-80 lg:text-100">
-    //       Raise your flag to tomorrow
-    //     </h2>
-    //   </article>
-    // </section>
-    <section className="all:ease-linear">
+    <section data-aos="aos-end" className="all:ease-linear">
       <article
-        data-aos="animate-slogun-bg"
-        className="relative flex justify-center h-[250vh] duration-200 z-20 bg-c-orange-300"
+        data-aos="aos-init"
+        className="relative flex justify-center h-[200vh] duration-200 z-20 bg-c-orange-300"
       >
-        <span
-          data-aos="animate-slogun"
-          className="sticky top-0 leading-[100vh] h-[100vh] duration-75 text-white text-[6em]"
-        >
+        <span className="sticky top-0 leading-[100vh] h-[100vh] duration-75 text-white text-[6em] font-open-sans font-bold">
           Raise your flag to tomorrow
         </span>
       </article>
